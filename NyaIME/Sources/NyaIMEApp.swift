57e74f5e1fdc8dd3f18d10e1692acb55fd3258e9
koki-develop <kou.pg.0131@gmail.com>
2024-09-06 20:55:19 +0900

    Create NSManualApplication and AppDelegate

--- NyaIME/Sources/NyaIMEApp.swift
+++ NyaIME/Sources/NyaIMEApp.swift
@@ -5,13 +5,29 @@
 //  Created by koki sato on 2024/09/06.
 //
 
-import SwiftUI
+import InputMethodKit
+
+class NSManualApplication: NSApplication {
+    let appDelegate = AppDelegate()
+
+    override init() {
+        super.init()
+        self.delegate = self.appDelegate
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
 
 @main
-struct NyaIMEApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var server = IMKServer()
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        self.server = IMKServer(
+            name: Bundle.main.infoDictionary?["InputMethodConnectionName"] as? String,
+            bundleIdentifier: Bundle.main.bundleIdentifier
+        )
     }
 }
