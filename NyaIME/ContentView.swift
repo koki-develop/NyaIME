0eccad2a11463ea6267786d960592a311f14797a
koki-develop <kou.pg.0131@gmail.com>
2024-09-06 17:08:55 +0900

    Create NyaIME project

---
+++ NyaIME/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NyaIME
+//
+//  Created by koki sato on 2024/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NyaIME/NyaIMEApp.swift
@@ -0,0 +1,17 @@
+//
+//  NyaIMEApp.swift
+//  NyaIME
+//
+//  Created by koki sato on 2024/09/06.
+//
+
+import SwiftUI
+
+@main
+struct NyaIMEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
