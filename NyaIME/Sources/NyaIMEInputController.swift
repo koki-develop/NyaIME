0278f257f1026a402232dc28004f455045581453
koki-develop <kou.pg.0131@gmail.com>
2024-09-06 21:15:27 +0900

    Implement InputController handling simply

--- NyaIME/Sources/NyaIMEInputController.swift
+++ NyaIME/Sources/NyaIMEInputController.swift
@@ -10,6 +10,18 @@ import InputMethodKit
 @objc(NyaIMEInputController)
 class NyaIMEInputController: IMKInputController {
     override func handle(_ event: NSEvent!, client sender: Any!) -> Bool {
+        guard let client = sender as? IMKTextInput else {
+            return false
+        }
+
+        if let text = event.characters {
+            client.insertText(
+                text.uppercased(),
+                replacementRange: NSRange(location: NSNotFound, length: NSNotFound)
+            )
+            return true
+        }
+
         return false
     }
 }
